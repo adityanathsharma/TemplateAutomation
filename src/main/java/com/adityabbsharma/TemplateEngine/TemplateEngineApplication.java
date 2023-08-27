d34da3c4c7a2483fadffafa992b124194d528151
adityabbsharma <adityabbsharma@gmail.com>
2023-08-27 15:44:22 +0530

    new springboot project added

---
+++ src/main/java/com/adityabbsharma/TemplateEngine/TemplateEngineApplication.java
@@ -0,0 +1,13 @@
+package com.adityabbsharma.TemplateEngine;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TemplateEngineApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TemplateEngineApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/adityabbsharma/TemplateEngine/TemplateEngineApplicationTests.java
@@ -0,0 +1,13 @@
+package com.adityabbsharma.TemplateEngine;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TemplateEngineApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
